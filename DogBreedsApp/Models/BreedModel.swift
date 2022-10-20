8f58e1579a387bbf5b55bd4c5464a970d041da3e
hastiranjkesh <hasti.ranjkesh@gmail.com>
2022-10-20 13:29:41 +0200

    Add a model for presenting the api response.

---
+++ DogBreedsApp/Models/BreedModel.swift
@@ -0,0 +1,13 @@
+//
+//  BreedModel.swift
+//  DogBreedsApp
+//
+//  Created by hasti ranjkesh on 10/20/22.
+//
+
+import Foundation
+
+struct Breed: Decodable {
+    let message: [String]
+    let status: String
+}
