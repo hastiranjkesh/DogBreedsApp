2cbfda40a1721b65377a37b8630e938e698649f7
hastiranjkesh <hasti.ranjkesh@gmail.com>
2022-10-20 18:04:21 +0200

    Add a collection view for showing a specific breed images.

---
+++ DogBreedsApp/Detail/BreedImageCollectionViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  BreedImageCollectionViewCell.swift
+//  DogBreedsApp
+//
+//  Created by hasti ranjkesh on 10/20/22.
+//
+
+import UIKit
+
+class BreedImageCollectionViewCell: UICollectionViewCell {
+    
+    static let cellIdentifier = "BreedImageCollectionViewCellId"
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: contentView.bounds)
+        imageView.clipsToBounds = true
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let btn = UIButton(frame: .zero)
+        btn.setImage(UIImage(systemName: "search"), for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        addSubview(imageView)
+        addSubview(likeButton)
+        
+        addConstraints()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            likeButton.heightAnchor.constraint(equalToConstant: 56),
+            likeButton.widthAnchor.constraint(equalToConstant: 56),
+            likeButton.trailingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: -16),
+            likeButton.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -16)
+        ])
+    }
+    
+    func setContent(imagePath: String) {
+        imageView.downloadImage(from: imagePath)
+        imageView.contentMode = .scaleAspectFill
+    }
+}
