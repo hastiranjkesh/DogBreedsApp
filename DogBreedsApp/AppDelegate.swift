f734897fba630c3ff4b69a5f38f82cc9e8caa6a8
hastiranjkesh <hasti.ranjkesh@gmail.com>
2022-10-20 13:31:11 +0200

    Display breed list view controller from appDelegate.

--- DogBreedsApp/AppDelegate.swift
+++ DogBreedsApp/AppDelegate.swift
@@ -10,10 +10,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let viewController = BreedListViewController(viewModel: BreedListViewModel(apiManager: ApiManager()))
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 }
